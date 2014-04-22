2c70a2f79efbb93b2ec05cfe531c9a7745419e62
weezer <weezer.su@gmail.com>
2014-04-23 01:25:46 +0800

    MadcLinkCheck_beta.py

--- testPydev/src/linkedlist/LinkedList.py
+++ testPydev/src/linkedlist/LinkedList.py
@@ -4,7 +4,7 @@ Created on Apr 7, 2014
 @author: Weezer
 '''
 
-class Node():
+class Node:
     def __init__(self, initiaData):
         self.data = initiaData
         self.next = None
@@ -18,10 +18,41 @@ class Node():
     def getNext(self):
         return self.next
     
-class LinkedList():
-    def __init__(self, head):
+class LinkedList:
+    def __init__(self):
         self.head = None
     def isEmpty(self):
         return self.head == None
-    def add(self, newNode):
+    def add(self, item):
+        temp = Node(item)
+        temp.setNext(self.head) 
+        self.head = temp
+        print self.head.getData()
+    def size(self):
+        current = self.head
+        count = 0
+        while current != None:
+#             print current.getNext().getData()
+            current = current.getNext()
+            count += 1
+        return count
+    def search(self, item):
+        current = self.head
+        found = False
+        while not found and current.getNext() != None:
+            if current.getData() != item:
+                current = current.getNext()
+            else:
+                found = True
+        return found
+    def remove(self, item):
+        previous = current = self.head
+        while current.getNext() != None:
+            if current.getData() != item:
+                previous = current
+                current = current.getNext()
+            else:
+                previous.setNext(current.getNext())
+                return "Deleted!"
+        return "Not Found"
         
