5e983f43be81c5255db89307972838610db79370
weezer <weezer.su@gmail.com>
2016-01-06 12:10:07 -0600

    Update pingpong.sh
    add the ease way to stop all process if you start too much
    kill -SIGQUIT `pgrep ping`

--- pingpong.sh
+++ pingpong.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # how to use? chmod +x pingpong.sh; ./pingpong.sh xxx.xxx.xxx.xxx
+# ease way to stop all process  kill -SIGQUIT `pgrep ping`
 
 NOW=$(date +"%m-%d-%Y")
 ping $1 | while read pong; do echo "$(date): $pong"; done > $1:"$NOW".log &
