370fb419576337b7d46197f34be690277e737271
weezer <weezer.su@gmail.com>
2016-01-14 16:22:05 -0600

    Update openstack_session.py

--- openstack_session.py
+++ openstack_session.py
@@ -2,13 +2,21 @@ from keystoneclient import session as ksc_session
 from keystoneclient.auth.identity import v3
 from keystoneclient.v3 import client as keystone_v3
 from novaclient.v2 import client as nova_v2
+import os
 
-auth = v3.Password(auth_url='http://172.29.236.100:5000/v3',
-                   username='admin',
-                   password='4IMuRmqNPITPwnhb9YUQtvbihErpCGKo',
-                   project_name='admin',
-                   user_domain_name='default',
-                   project_domain_name='default')
+def get_keystone_creds():
+    d = {}
+    d['username'] = os.environ['OS_USERNAME']
+    d['password'] = os.environ['OS_PASSWORD']
+    d['auth_url'] = os.environ['OS_AUTH_URL']
+    d['project_name'] = os.environ['OS_PROJECT_NAME']
+    d['user_domain_name'] = os.environ['OS_USER_DOMAIN_NAME']
+    d['project_domain_name'] = os.environ['OS_PROJECT_DOMAIN_NAME']
+    return d
+
+cred = get_keystone_creds()
+
+auth = v3.Password(**cred)
 
 session = ksc_session.Session(auth=auth)
 print "test"
